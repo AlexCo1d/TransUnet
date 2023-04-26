5170c21411225a489f7b0466b00b513450b57564
alexyhzou <alexyhzou@qq.com>
2023-04-26 22:56:37 +0800

    little amendment

--- utils/metrics.py
+++ utils/metrics.py
@@ -21,7 +21,7 @@ def CE_Loss(inputs, target, cls_weights, num_classes=2):
     temp_inputs = inputs.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
     temp_target = target.view(-1)
 
-    CE_loss = nn.CrossEntropyLoss(weight=torch.tensor(cls_weights), ignore_index=num_classes)(temp_inputs, temp_target)
+    CE_loss = nn.CrossEntropyLoss(weight=cls_weights, ignore_index=num_classes)(temp_inputs, temp_target)
     return CE_loss
 
 
