d44e08639e1311787ef1af6cc29ddc1bbe2fbdcd
alexyhzou <alexyhzou@qq.com>
2023-04-27 15:47:42 +0800

    little amendment

--- predict.py
+++ predict.py
@@ -5,35 +5,34 @@
 from PIL import Image
 import os
 import time
-from unet import uNet as psp
 from torch.autograd import Variable
 import torch.nn.functional as F
 import numpy as np
 import torch
 
 
-class miou_Pspnet(psp):
-    def detect_image(self, image):
-        orininal_h = np.array(image).shape[0]
-        orininal_w = np.array(image).shape[1]
-
-        image, nw, nh = self.letterbox_image(image, (self.model_image_size[1], self.model_image_size[0]))
-        images = [np.array(image) / 255]
-        images = np.transpose(images, (0, 3, 1, 2))
-
-        with torch.no_grad():
-            images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
-            if self.cuda:
-                images = images.cuda()
-            pr = self.net(images)[0]
-            pr = F.softmax(pr.permute(1, 2, 0), dim=-1).cpu().numpy().argmax(axis=-1)
-
-        pr = pr[int((self.model_image_size[0] - nh) // 2):int((self.model_image_size[0] - nh) // 2 + nh),
-             int((self.model_image_size[1] - nw) // 2):int((self.model_image_size[1] - nw) // 2 + nw)]
-
-        image = Image.fromarray(np.uint8(pr)).resize((orininal_w, orininal_h), Image.NEAREST)
-
-        return image
+# class miou_Pspnet(psp):
+#     def detect_image(self, image):
+#         orininal_h = np.array(image).shape[0]
+#         orininal_w = np.array(image).shape[1]
+#
+#         image, nw, nh = self.letterbox_image(image, (self.model_image_size[1], self.model_image_size[0]))
+#         images = [np.array(image) / 255]
+#         images = np.transpose(images, (0, 3, 1, 2))
+#
+#         with torch.no_grad():
+#             images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
+#             if self.cuda:
+#                 images = images.cuda()
+#             pr = self.net(images)[0]
+#             pr = F.softmax(pr.permute(1, 2, 0), dim=-1).cpu().numpy().argmax(axis=-1)
+#
+#         pr = pr[int((self.model_image_size[0] - nh) // 2):int((self.model_image_size[0] - nh) // 2 + nh),
+#              int((self.model_image_size[1] - nw) // 2):int((self.model_image_size[1] - nw) // 2 + nw)]
+#
+#         image = Image.fromarray(np.uint8(pr)).resize((orininal_w, orininal_h), Image.NEAREST)
+#
+#         return image
 
 
 def show_image():
@@ -52,7 +51,8 @@ def show_image():
 
 
 def transfer_image():
-    psp = miou_Pspnet()
+    from unet import uNet
+    unet = uNet()
     test_path = './for_test'
     label_path = os.path.join(test_path, 'label')
     image_path = os.path.join(test_path, 'image')
@@ -76,7 +76,7 @@ def transfer_image():
         #
         image[image!=0]=set_label
         image=Image.fromarray(image)
-        image = psp.detect_image(image,mix=False)
+        image = unet.detect_image(image,mix=False)
         # image = image.resize((512, 512))
         image.save(f"miou_pr_dir/{image_name}")
         print(image_name, " done!")
