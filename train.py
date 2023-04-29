da52673b7d73e44b15f390b7d40cae46a0aa88af
alexyhzou <alexyhzou@qq.com>
2023-04-29 17:46:48 +0800

    little amendment

--- train.py
+++ train.py
@@ -303,27 +303,28 @@ if __name__ == "__main__":
     #   权值文件的下载请看README
     #   权值和主干特征提取网络一定要对应
     # -------------------------------------------#
-    # model_path = r"model_data/pspnet_mobilenetv2.pth"
-    # # 加快模型训练的效率
-    # print('Loading weights into state dict...')
-    # model_dict = model.state_dict()
-    # pretrained_dict = torch.load(model_path, map_location=device)
-    # load_key, no_load_key, temp_dict = [], [], {}
-    # for k, v in pretrained_dict.items():
-    #     if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
-    #         temp_dict[k] = v
-    #         load_key.append(k)
-    #     else:
-    #         no_load_key.append(k)
-    # model_dict.update(temp_dict)
-    # model.load_state_dict(model_dict)
-    # # ------------------------------------------------------#
-    # #   显示没有匹配上的Key
-    # # ------------------------------------------------------#
-    # if local_rank == 0:
-    #     print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
-    #     print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
-    #     print("\n\033[1;33;44m温馨提示，head部分没有载入是正常现象，Backbone部分没有载入是错误的。\033[0m")
+    if pretrained:
+        model_path = r"model_data/pspnet_mobilenetv2.pth"
+        # 加快模型训练的效率
+        print('Loading weights into state dict...')
+        model_dict = model.state_dict()
+        pretrained_dict = torch.load(model_path, map_location=device)
+        load_key, no_load_key, temp_dict = [], [], {}
+        for k, v in pretrained_dict.items():
+            if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
+                temp_dict[k] = v
+                load_key.append(k)
+            else:
+                no_load_key.append(k)
+        model_dict.update(temp_dict)
+        model.load_state_dict(model_dict)
+        # ------------------------------------------------------#
+        #   显示没有匹配上的Key
+        # ------------------------------------------------------#
+        if local_rank == 0:
+            print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
+            print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
+            # print("\n\033[1;33;44m温馨提示，head部分没有载入是正常现象，Backbone部分没有载入是错误的。\033[0m")
 
     # ----------------------#
     #   记录Loss
