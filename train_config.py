8c14740efbbb20c65338035605e1ce1037576679
alexyhzou <alexyhzou@qq.com>
2023-05-04 01:51:52 +0800

    little amendment

--- train_config.py
+++ train_config.py
@@ -60,8 +60,8 @@ class Config:
     model_path = './model_data/pretrained_weight.pth'
     # no_load_dict,加载预训练时不加载解码器部分
     no_load_dict = ['decoder', 'segmentation_head']
-    frozen_modules = ["cbam", "decoder", 'ASPP_unit1', 'ASPP_unit2', 'ASPP_unit3', 'segmentation_head',
-                           'cls']  # removed: 'cls','ASPP_unit1', 'ASPP_unit2'
+    frozen_modules = ["cbam", "decoder", 'ASPP_unit1', 'ASPP_unit2', 'ASPP_unit3', 'segmentation_head', 'se',
+                      'cls']  # removed: 'cls','ASPP_unit1', 'ASPP_unit2'
 
     # ----------------------#
     #   记录Loss
@@ -71,12 +71,12 @@ class Config:
     Interval_Epoch = 150
     # 设置冻结的epoch
     Freeze_Epoch = 40
-    Freeze_Batch_Size=2
+    Freeze_Batch_Size = 2
 
     # ----------------------#
     # 输出图像的形式
     # 0：不上色
     # 1：上色
     # 2：混合
-    #-----------------------#
-    output_type=2
+    # -----------------------#
+    output_type = 2
