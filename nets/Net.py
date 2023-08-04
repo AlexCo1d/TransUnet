c9e7f993d18520a3fbd4d2376359a1f3533969f0
AlexCo1d <2242062912@qq.com>
2023-08-04 11:38:45 +0800

    little amendment

--- nets/Net.py
+++ nets/Net.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import functools
 import torch.nn.functional as F
 
-from nets.vit_cbam_aspp_modeling import Vit_CBAM, Vit_CBAM_ASPP
+from nets.vit_cbam_aspp_modeling import Vit_CBAM_ASPP
 from nets.vit_cbam_modeling import Vit_CBAM, Vit_CBAM_CBAM
 from .transunet_modeling import VisionTransformer as ViT_seg
 from .transunet_modeling import CONFIGS as CONFIGS_ViT_seg
